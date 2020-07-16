cbe51bca32b70d7f21128f5d2c695ccdfb9caa47
jules2m1998 <mevaajules9@gmail.com>
2020-07-16 14:33:23 +0100

    first

---
+++ components/topFooter/TopFooter.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "TopFooter"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/topFooter/TopFooterItem.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "TopFooterItem"
+    }
+</script>
+
+<style scoped>
+
+</style>
