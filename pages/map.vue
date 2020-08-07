41175c84a6ee63ef289cb235f0c458d1d5c2920c
jules2m1998 <mevaajules9@gmail.com>
2020-08-07 14:39:48 +0100

    Add map

---
+++ pages/map.vue
@@ -0,0 +1,16 @@
+<template>
+  <google-map />
+</template>
+
+<script>
+import GoogleMap from '@/components/google-map/GoogleMap'
+export default {
+  name: 'Map',
+  components: { GoogleMap },
+  layout: 'weird'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ plugins/google-map.js
@@ -0,0 +1,6 @@
+import Vue from 'vue'
+import * as VueGoogleMaps from 'vue2-google-maps'
+
+Vue.use(VueGoogleMaps, {
+  load: { key: 'AIzaSyD5hAoNF0LnK4NpUtubFmda_Jhn-qELfm8' }
+})
