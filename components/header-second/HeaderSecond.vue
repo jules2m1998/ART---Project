18793f1a1891e8ac25baae018cd7ef53cf278e3c
jules2m1998 <mevaajules9@gmail.com>
2020-09-16 09:31:12 +0100

    Update

--- components/header-second/HeaderSecond.vue
+++ components/header-second/HeaderSecond.vue
@@ -1,8 +1,33 @@
 <template>
   <div class="header-second" data-sticky>
     <v-container>
-      <header-logo />
-      <header-btn />
+      <div class="top" data-inme>
+        <header-logo />
+        <div style="display: flex; flex-direction: row;">
+          <header-btn data-disappear style="padding-right: 12px" />
+          <div data-appear style="min-height: 100px; padding-left: 12px;padding-right: 12px;">
+            <div class="pulse-btn" style="position: relative; top: 26px;">
+              <v-btn
+                id="profile-btn"
+                width="40"
+                height="40"
+                fab
+                dark
+                large
+                color="white"
+                depressed
+              >
+                <v-icon color="black" size="20">
+                  fas fa-bars
+                </v-icon>
+              </v-btn>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="bottom">
+        <search data-search data-mount />
+      </div>
     </v-container>
   </div>
 </template>
@@ -11,26 +36,34 @@
 import HeaderLogo from '@/components/header-logo/HeaderLogo'
 import HeaderBtn from '@/components/header-btn/HeaderBtn'
 import sticky from '@/mixins/sticky'
+import Search from '@/components/search/Search'
 export default {
   name: 'HeaderSecond',
-  components: { HeaderBtn, HeaderLogo },
+  components: { Search, HeaderBtn, HeaderLogo },
   mixins: [sticky]
 }
 </script>
 
 <style lang="scss" scoped>
-  .header-second{
-    background: url("/bg.jpg") no-repeat;
-    .container{
-      display: flex;
-      flex-direction: row;
-      justify-content: space-between;
-      align-items: center;
-    }
+.header-second{
+  background: url("/bg.jpg") no-repeat;
+  .top{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    overflow-x: hidden;
   }
-  .sticky{
-    position: fixed;
-    top: 0;
-    z-index: 15;
+  .bottom {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    width: 100%;
   }
+}
+.sticky{
+  position: fixed;
+  top: 0;
+  z-index: 15;
+}
 </style>
--- mixins/sticky.js
+++ mixins/sticky.js
@@ -3,14 +3,27 @@ export default {
     element: undefined,
     fake: undefined,
     rect: null,
-    top: null
+    top: null,
+    inme: undefined,
+    disappear: undefined,
+    appear: undefined,
+    mount: undefined,
+    parentMount: undefined,
+    mountParentLn: null
   }),
   methods: {
+    /**
+     * Renvoie le nombre de scroll de la page
+     * @return {Number}
+     */
     scrollY () {
       const supportPageOffset = window.pageXOffset !== undefined
       const isCSS1Compat = ((document.compatMode || '') === 'CSS1Compat')
       return supportPageOffset ? window.pageYOffset : isCSS1Compat ? document.documentElement.scrollTop : document.body.scrollTop
     },
+    /**
+     * Applique le comportement sticky lors du scroll et anime les élément
+     */
     onscroll () {
       const hasScrollClass = this.element.classList.contains('sticky')
       if (this.scrollY() > this.top && !hasScrollClass) {
@@ -21,8 +34,29 @@ export default {
         this.element.classList.remove('sticky')
         this.element.parentNode.removeChild(this.fake)
       }
-      console.log(this.scrollY() <= this.top, this.scrollY(), this.top)
+      const nbScroll = this.scrollY() < this.mountParentLn ? this.scrollY() : this.mountParentLn
+      const parentHeight = this.scrollY() < this.mountParentLn ? (this.mountParentLn - this.scrollY()) !== 0 ? (this.mountParentLn - this.scrollY()) + 'px' : 'auto' : '0'
+      const parentMountH = this.mount.parentNode.getBoundingClientRect().height
+      // const transpourcent = parentMountH / this.mountParentLn * 100 > 0 ? parentMountH / this.mountParentLn * 100 : 0
+      let transAppear
+      let transDisAppear
+      if (this.scrollY() > 0) {
+        transAppear = parentMountH * this.appear.getBoundingClientRect().width / this.mountParentLn
+        transDisAppear = parentMountH * this.appear.getBoundingClientRect().width / this.mountParentLn
+      } else {
+        transAppear = 100
+        transDisAppear = 100
+      }
+
+      this.mount.style.transform = `translate3d(0, -${nbScroll}px, 0)`
+      this.mount.parentNode.style.height = parentHeight
+
+      this.appear.style.transform = `translate3d(-${100 - transAppear}%,0,0)`
+      this.disappear.style.transform = `translate3d(${100 - transDisAppear}%,0,0)`
     },
+    /**
+     * Permet la mise en responsive
+     */
     onresize () {
       this.element.style.width = 'auto'
       this.element.classList.remove('sticky')
@@ -33,6 +67,13 @@ export default {
       this.fake.style.height = this.rect.height + 'px'
       this.fake.style.display = 'block'
       this.onscroll()
+    },
+    /**
+     * Renvoie la hauteur du parent du mount
+     * @return {number | number}
+     */
+    parentMountBounding () {
+      return this.inme.getBoundingClientRect().height
     }
   },
   mounted () {
@@ -43,6 +84,16 @@ export default {
     this.fake = document.createElement('div')
     this.fake.style.width = this.rect.width + 'px'
     this.fake.style.height = this.rect.height + 'px'
+
+    this.inme = document.querySelector('[data-inme]')
+    this.disappear = document.querySelector('[data-disappear]')
+    this.appear = document.querySelector('[data-appear]')
+    this.mount = document.querySelector('[data-mount]')
+    this.parentMount = this.mount.parentNode
+
+    this.appear.parentNode.style.transform = `translate3d( ${this.appear.getBoundingClientRect().width}px,0,0)`
+    this.mountParentLn = this.parentMountBounding()
+
     window.addEventListener('scroll', this.onscroll.bind(this))
     window.addEventListener('resize', this.onresize.bind(this))
   }
